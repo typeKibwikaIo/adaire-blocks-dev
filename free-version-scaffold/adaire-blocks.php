0dfafc7f8d5e22863f7d8008cf6730be68cab9c7
modamouth <lnnondabula@gmail.com>
2026-06-22 13:29:57 +0200

    Fix Advanced Feature Grid icons: add to Bootstrap Icons frontend enqueue check

--- free-version-scaffold/adaire-blocks.php
+++ free-version-scaffold/adaire-blocks.php
@@ -140,7 +140,8 @@ function enqueue_bootstrap_icons_assets() {
         has_block( 'create-block/icon-box-block', $post ) ||
         has_block( 'create-block/social-banner-block', $post ) ||
         has_block( 'create-block/social-share-block', $post ) ||
-        has_block( 'create-block/our-process-block', $post )
+        has_block( 'create-block/our-process-block', $post ) ||
+        has_block( 'create-block/infogrid-2-block', $post )
     ) {
         // Enqueue Bootstrap Icons CSS from CDN
         wp_enqueue_style(
