bb79c6425a1c5ffc37a1c5c9c98d822b1ccdd85f
Dan Kibwika <dan.kibwika@adaire.com>
2026-09-09 16:53:10 +0200

    1.3.4

--- adaire-blocks.php
+++ adaire-blocks.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name:       Adaire Blocks
  * Description:       A powerful WordPress plugin that helps developers and designers create visually stunning, high-performance websites with ease right inside the Gutenberg editor.
- * Version:           Guten-Blocks-release-1.3.4
+ * Version:           1.3.4
  * Requires at least: 6.7
  * Requires PHP:      7.0
  * Author:            <a href="https://adaireblocks.com" target="_blank">Adaire Digital</a>
@@ -210,7 +210,8 @@ add_action(
 // End of version rollback code
 
 // Define plugin constants
-define('ADAIRE_BLOCKS_VERSION', 'Guten-Blocks-release-1.3.4');
+define('ADAIRE_BLOCKS_VERSION', '1.3.4');
+define('ADAIRE_BLOCKS_PLUGIN_URL', plugin_dir_url(__FILE__));
 define('ADAIRE_BLOCKS_PLUGIN_PATH', plugin_dir_path(__FILE__));
 define('ADAIRE_BLOCKS_PLUGIN_FILE', __FILE__);
 
